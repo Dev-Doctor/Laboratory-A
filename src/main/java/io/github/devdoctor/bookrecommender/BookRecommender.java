c60c35635459e09eff110ccec47e0fd93e4846f6
DevDoctor <84864508+Dev-Doctor@users.noreply.github.com>
2025-02-05 13:52:05 +0100

    Probably last push berefore Lab-B

--- src/main/java/io/github/devdoctor/bookrecommender/BookRecommender.java
+++ src/main/java/io/github/devdoctor/bookrecommender/BookRecommender.java
@@ -34,22 +34,33 @@ public class BookRecommender extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        // load the main window layout
         FXMLLoader fxmlLoader = new FXMLLoader(BookRecommender.class.getResource(START_WINDOW.resource + ".fxml"));
+        // load the css for the window
         String css = this.getClass().getResource(DEFAULT_CSS).toExternalForm();
+        // set the window icon
         stage.getIcons().add(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream(ICON))));
+        // start the scene
         Scene scene = new Scene(fxmlLoader.load(), START_WINDOW.width, START_WINDOW.height);
+        // add the style to the scene
         scene.getStylesheets().add(css);
+        // set the title of the window
         stage.setTitle(START_WINDOW.title);
+        // set the current scene to the newly created
         stage.setScene(scene);
+        // show the scene
         stage.show();
     }
 
     public static void main(String[] args) {
-        inizialization();
+        initialization();
         launch();
     }
 
-    private static void inizialization() {
+    /**
+     * Sets the default state of the program
+     */
+    private static void initialization() {
         LoadedData.loginEvent = new LoginEvent();
         LoadedData.userEvent = new UpdateUserEvent();
         FileUtils.loadConfig();
